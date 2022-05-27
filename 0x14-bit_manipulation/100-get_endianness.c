0019250366647e9e029aa760720c4414c369e719
AdexDaba <adexdaba23@gmail.com>
2022-05-27 05:08:04 -0700

    100vin

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,25 @@
+#include <stdio.h>
 #include "main.h"
 
-/**int check_for_endianness()
+int main(void)
+{
+    int n;
+
+    n = get_endianness();
+    if (n != 0)
+    {
+        printf("Little Endian\n");
+    }
+    else
+    {
+        printf("Big Endian\n");
+    }
+    return (0);
+}
+
+int check_for_endianness()
 {
   unsigned int x = 1;
   char *c = (char*) &x;
   return (int)*c;
 }
-**/
