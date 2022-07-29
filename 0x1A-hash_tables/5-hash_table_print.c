1fb0212e7ab2e7f484c8b7cbf9f202416836491b
AdexDaba <adexdaba23@gmail.com>
2022-07-29 02:21:00 -0700

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,38 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print - Prints a hash table.
- * @ht: A pointer to the hash table to print.
- *
- * Description: Key/value pairs are printed in the order
- *              they appear in the array of the hash table.
+ * hash_table_print - prints the hash table
+ * @ht: the hash table of hash_table_print
+ * Return: nothing
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned long int i;
-	unsigned char comma_flag = 0;
+	int i;
+	hash_node_t *item;
+	int first_flag = 0;
 
 	if (ht == NULL)
 		return;
 
 	printf("{");
-	for (i = 0; i < ht->size; i++)
+	for (i = 0; i < (int)ht->size; i++)
 	{
-		if (ht->array[i] != NULL)
+		item = ht->array[i];
+		if (item == NULL)
+			continue;
+		if (first_flag <= 1)
+			first_flag++;
+		while (item)
 		{
-			if (comma_flag == 1)
+			if (first_flag == 2)
 				printf(", ");
-
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
-				if (node != NULL)
-					printf(", ");
-			}
-			comma_flag = 1;
+			printf("'%s': '%s'", item->key, item->value);
+			item = item->next;
 		}
 	}
 	printf("}\n");
