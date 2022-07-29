1e57452a63ec6ba8f985c8686f74e18373812212
AdexDaba <adexdaba23@gmail.com>
2022-07-29 02:19:22 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,28 +1,31 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieve the value associated with
- *                  a key in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to get the value of.
- *
- * Return: If the key cannot be matched - NULL.
- *         Otherwise - the value associated with key in ht.
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table data structure of hash_table_t
+ * @key: the key, string
+ * Return: the value of the associated key or NULL if not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	hash_node_t *node;
 	unsigned long int index;
+	hash_node_t *found_node = NULL;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if (index >= ht->size)
+	found_node = ht->array[index];
+
+	if (found_node == NULL)
 		return (NULL);
 
-	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-	return ((node == NULL) ? NULL : node->value);
+	while (found_node)
+	{
+		if (strcmp(found_node->key, key) == 0)
+			return (found_node->value);
+		found_node = found_node->next;
+	}
+
+	return (NULL);
 }
