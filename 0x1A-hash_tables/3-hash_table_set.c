2ec312100262307954066cc2705868c52107284a
AdexDaba <adexdaba23@gmail.com>
2022-07-29 02:17:49 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,44 +1,44 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Add or update an element in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to add - cannot be an empty string.
- * @value: The value associated with key.
- *
- * Return: Upon failure - 0.
- *         Otherwise - 1.
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table data structure of hash_table_t
+ * @key: the key string
+ * @value: the value corresponding to the key
+ * Return: 1 if it succeeded, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;
-	unsigned long int index, i;
+	hash_node_t *new_node = NULL, *tmp = NULL;
+	unsigned long int index = 0;
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-		return (0);
-
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0
+	    || key == NULL || strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			free(tmp->value);
+			tmp->value = strdup((char *)value);
+			if (tmp->value == NULL)
+				return (0);
 			return (1);
 		}
+		tmp = tmp->next;
 	}
 
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-		free(value_copy);
+	new_node = malloc(sizeof(new_node));
+	if (new_node == NULL)
 		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
 
+	new_node->key = strdup((char *)key);
+	new_node->value = strdup((char *)value);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
